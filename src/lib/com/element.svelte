69d8b767c770179bead854877d9a7ad94b346747
djely2005 <elyass162005@gmail.com>
2024-11-09 21:17:13 +0000

    Bug to fix => bracketCalculation function. futur plan => - generate enough element to do a bracket - space and link them - random generation + seeded one

---
+++ src/lib/com/element.svelte
@@ -0,0 +1,10 @@
+<script>
+    let {text, height=$bindable(), width=$bindable(), finalHeight, finalWidth} = $props();
+</script>
+
+<div class="xl:rounded-full xl:bg-slate-700 xl:relative xl:flex xl:justify-center xl:items-center xl:text-center w-fit" bind:offsetHeight={height} bind:offsetWidth={width} style:height = '{finalHeight}px' style:width = '{finalWidth}px'>
+    <div>
+
+    </div>
+    <p class="xl:text-white xl:text-3xl xl:font-bold xl:px-12 xl:py-6">{text}</p>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+    import { onMount } from "svelte";
+    import Element from "../lib/com/element.svelte";
+    function bracketCalculation(array){
+        let howMany;
+        loop: for (let index = 0; index < array.length; index++) {
+            const element = array[index];
+            if(2**index > array.length){
+                howMany = 2**index;
+                break loop;
+            }
+        }
+        if(array.lenght == 0){
+            howMany = 0;
+        }
+        return howMany;
+    }
+    function getBiggest(array){
+        return Math.max.apply(Math, array);
+    }
+    let teams = $state([
+        {text: 'Team1 long name', height: 0, width: 0},
+        {text: 'Team2', height: 0, width: 0},
+        {text: 'Team3', height: 0, width: 0}
+    ]);
+    console.log(bracketCalculation(teams));
+    let heights = [], widths = [];
+    let [finalHeight, finalWidth] = $state(0);
+    $effect(()=>{
+        teams.forEach(element => {
+            heights.push(element.height);
+            widths.push(element.width);
+        });
+        console.log(widths);
+        finalHeight = getBiggest(heights);
+        finalWidth = getBiggest(widths);
+    });
+</script>
+
+{#each teams as team}
+    <div class="">
+        <Element text = {team.text} bind:height = {team.height} bind:width = {team.width} finalWidth = {finalWidth} finalHeight={finalHeight}/>
+    </div>
+{/each}
